76fafe333b25b3989aa0c753d49c8b99b8b6d4b4
ZetLink <royalezetlink@gmail.com>
2025-10-01 13:31:46 +0000

    rhode: add utils

--- device.mk
+++ device.mk
@@ -7,12 +7,44 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
 
-# Dynamic partitions
+# A/B support
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS ?= boot vendor_boot dtbo vbmeta vbmeta_system super
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Boot control hal for A/B
-PRODUCT_PACKAGES += android.hardware.boot@1.1-impl-qti android.hardware.boot@1.1-impl-qti.recovery android.hardware.boot@1.1-service
+PRODUCT_PACKAGES += android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service update_engine \
+    fastbootd
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# f2fs utilities
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 # Bootctrl Bin
 PRODUCT_PACKAGES += \
